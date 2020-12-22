fc4201b111c11f1d4a8459d60bf22621a94a9c4e
Alex Shortt <alexander.shortt@gmail.com>
2020-12-22 00:56:53 -0800

    setup template

---
+++ src/pages/kirax23.tsx
@@ -0,0 +1,67 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+
+const Gotham = dynamic(import("scenes/Gotham"), { ssr: false });
+
+const ARTIST = {
+  name: "KIRA-X23",
+  socials: [
+    "https://soundcloud.com/experiment23",
+    "https://twitter.com/djkirax23",
+    "https://www.instagram.com/djkirax23/",
+  ],
+};
+
+const url = `https://d27rt3a60hh1lx.cloudfront.net/content/muse.place/kirax23`;
+const artwork = [
+  {
+    src: `${url}/1.jpg`,
+  },
+  {
+    src: `${url}/2.jpg`,
+  },
+  {
+    src: `${url}/3.jpg`,
+  },
+  {
+    src: `${url}/4.jpg`,
+  },
+  {
+    src: `${url}/5.jpg`,
+  },
+  {
+    src: `${url}/6.jpg`,
+  },
+];
+
+const LinkTree: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>KIRA-X23 | Muse Place</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,300&family=Space+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Gotham
+        artwork={artwork}
+        socials={ARTIST.socials}
+        name={ARTIST.name}
+        map="city"
+        scenePos={[0, -20, 0]}
+        fogNear={0}
+        fogFar={150}
+        fogColor={"#000000"}
+        hMapScale={30}
+        xzMapScale={100}
+        floorColor="black"
+        night
+        stars
+      />
+    </>
+  );
+};
+
+export default LinkTree;
