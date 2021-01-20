4526f94099d87aed44b72955118c8cd9997ed9ef
Isaiah <anyiminene@gmail.com>
2021-01-20 02:41:42 -0800

    audioreactive among us w/slight workaround

--- src/scenes/Alto/AudioReactive/ReactivePrimitive.tsx
+++ src/scenes/Alto/AudioReactive/ReactivePrimitive.tsx
@@ -1,9 +1,7 @@
-import { useMemo, useRef } from "react";
+import { useMemo, useRef, Suspense } from "react";
 import { useFrame, useLoader } from "react-three-fiber";
 import * as THREE from "three";
-import Knot from "../../../themes/Renaissance/models/Knot";
-import { ModifierStack, Taper } from "three.modifiers/src/index";
-import { BufferGeometry } from "three";
+import Amongus from "../../../themes/Alto/models/Amongus";
 
 type ReactiveProps = JSX.IntrinsicElements["group"] & {
   url?: string;
@@ -30,12 +28,11 @@ const ReactivePrimitive = (props: ReactiveProps) => {
   const innerGroup = useRef<THREE.Group>();
   const material = useRef<THREE.MeshStandardMaterial>();
   const freqIndex = useRef(Math.floor(Math.random() * 16));
-  const mesh = useRef<THREE.Mesh | undefined>();
+  const amongus = useRef<any>();
 
   useFrame(({ clock }, delta) => {
+    const freqData = aa?.getFrequencyData()[freq || freqIndex.current] || 0;
     if (material.current) {
-      const freqData = aa?.getFrequencyData()[freq || freqIndex.current] || 0;
-
       if (freqData < min) {
         min = freqData;
       }
@@ -51,28 +48,30 @@ const ReactivePrimitive = (props: ReactiveProps) => {
       material.current.displacementScale = Math.max(0, disp / 2 + 0.05);
     }
 
+    if (amongus.current) {
+      amongus.current.traverse((child: any) => {
+        if (child instanceof THREE.Mesh) {
+          if (freqData < min) {
+            min = freqData;
+          }
+
+          if (freqData > max) {
+            max = freqData;
+          }
+
+          const modFreqData = (freqData - min) / (max - min);
+
+          const disp = THREE.MathUtils.lerp(MIN_DISP, MAX_DISP, modFreqData);
+          // child.material.displacementMap = {texture}
+          child.material.displacementScale = Math.max(0, disp / 2 + 0.05);
+        }
+      });
+    }
+
     if (group.current) {
       group.current.rotation.x = clock.getElapsedTime() / (7 + seed * 30);
       group.current.rotation.y = clock.getElapsedTime() / (10 + seed * 30);
       group.current.rotation.z = clock.getElapsedTime() / (9 + seed * 30);
-    }
-
-    if (mesh.current) {
-      const meshBuf = mesh?.current?.geometry as BufferGeometry;
-      const vertices = meshBuf.attributes.position.array;
-      const count = meshBuf.attributes.position.count;
-      for (let i = 0; i < count; i++) {
-        const x = vertices[i * 3];
-        const y = vertices[i * 3 + 1];
-        const z = vertices[i * 3 + 2];
-        if (i < count / 2) {
-          meshBuf.attributes.position.setZ(
-            i,
-            z + delta * Math.sin(clock.getElapsedTime())
-          );
-        }
-      }
-      meshBuf.attributes.position.needsUpdate = true;
     }
   });
 
@@ -82,7 +81,7 @@ const ReactivePrimitive = (props: ReactiveProps) => {
     <group scale={[SCALE, SCALE, SCALE]} position={[0, 0, 0]}>
       <group {...props} ref={group}>
         <group ref={innerGroup}>
-          <mesh ref={mesh}>
+          <mesh>
             <sphereBufferGeometry args={[2, 50, 50]} />
             <meshStandardMaterial
               ref={material}
@@ -92,10 +91,9 @@ const ReactivePrimitive = (props: ReactiveProps) => {
               displacementScale={0}
             />
           </mesh>
-          <mesh scale={[1 + MIN_DISP, 1 + MIN_DISP, 1 + MIN_DISP]}>
-            <boxBufferGeometry args={[2, 2, 2, 40, 40, 40]} />
-            <meshBasicMaterial color="black" />
-          </mesh>
+          <Suspense fallback={null}>
+            <Amongus texture={texture} position-y={-3} ref={amongus} />
+          </Suspense>
         </group>
       </group>
     </group>
