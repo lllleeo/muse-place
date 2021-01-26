d0d1c5a1be959f5a523f524b833de8e09d5bb760
Alex Shortt <alexander.shortt@gmail.com>
2021-01-26 01:15:49 -0800

    swap tablatures, cleanup muse credits

--- src/themes/Alto/components/Tablatures.tsx
+++ src/themes/Alto/components/Tablatures.tsx
@@ -1,6 +1,7 @@
 import { Text } from "@react-three/drei";
 import React from "react";
 import SocialLinks from "../../components/SocialLinks";
+import SocialButton from "../../components/SocialButton";
 import Scroll from "../models/Scroll";
 import { Floating } from "spacesvr";
 
@@ -26,7 +27,38 @@ const Tablatures = (props: Props) => {
         rotation={[0, Math.PI / 6, 0]}
         name="tablature-left"
       >
-        <group name="collection" position-y={-0.5}>
+        <group position-y={-0.05}>
+          <Text {...textStyles} textAlign="justify">
+            Welcome to Alto
+          </Text>
+          <Text
+            {...textStyles}
+            fontSize={0.113}
+            position-y={-0.31}
+            textAlign="justify"
+          >
+            otherwise known as the music venue of the gods. For thousands of
+            years, Alto has been only accessible to immortal beings, but you
+            seem to have found us anyway.. At the top of those stairs, you'll be
+            able the first mortal to ever hear Lucid Monday's latest work.
+          </Text>
+        </group>
+        <group position-y={-1.5}>
+          <Text {...textStyles} fontSize={0.125} textAlign="center">
+            Follow Lucid Monday
+          </Text>
+          <group scale={[1.75, 1.75, 1.75]} position-y={-0.28}>
+            <SocialLinks socials={socials} />
+          </group>
+        </group>
+      </group>
+
+      <group
+        position={[1.93, 2.2, 29.26]}
+        rotation={[0, -Math.PI / 6, 0]}
+        name="tablature-right"
+      >
+        <group name="collection" position-y={-0.4}>
           <Floating height={0.05} speed={2}>
             <Scroll
               open={false}
@@ -52,44 +84,30 @@ const Tablatures = (props: Props) => {
             collected
           </Text>
         </group>
-        <Text
-          {...textStyles}
-          position-y={-1.8}
-          fontSize={0.125}
-          textAlign="center"
-        >
-          Created By Muse • Get Your Own
-        </Text>
-      </group>
-
-      <group
-        position={[1.93, 2.2, 29.26]}
-        rotation={[0, -Math.PI / 6, 0]}
-        name="tablature-right"
-      >
-        <group position-y={-0.05}>
-          <Text {...textStyles} textAlign="justify">
-            Welcome to Alto
+        <group name="muse-credits" position-y={0.1} position-x={0.05}>
+          <Text
+            {...textStyles}
+            position={[-0.22, -1.65, 0]}
+            fontSize={0.11}
+            letterSpacing={0.19}
+            textAlign="right"
+          >
+            Created By Muse
           </Text>
           <Text
             {...textStyles}
-            fontSize={0.113}
-            position-y={-0.31}
-            textAlign="justify"
+            position={[-0.22, -1.8, 0]}
+            fontSize={0.11}
+            letterSpacing={0.19}
+            textAlign="right"
           >
-            otherwise known as the music venue of the gods. For thousands of
-            years, Alto has been only accessible to immortal beings, but you
-            seem to have found us anyway.. At the top of those stairs, you'll be
-            able the first mortal to ever hear Lucid Monday's latest work.
+            Want Your Own?
           </Text>
-        </group>
-        <group position-y={-1.5}>
-          <Text {...textStyles} fontSize={0.125} textAlign="center">
-            Follow Lucid Monday
-          </Text>
-          <group scale={[1.75, 1.75, 1.75]} position-y={-0.28}>
-            <SocialLinks socials={socials} />
-          </group>
+          <SocialButton
+            link="https://musevr.typeform.com/to/QwGYwJH2"
+            position={[0.45, -1.785, 0]}
+            scale={[0.6, 0.6, 0.6]}
+          />
         </group>
       </group>
     </>
