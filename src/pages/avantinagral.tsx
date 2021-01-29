e390f13e4f42c54277d63574e3a4dc0195f28635
benjaminha14 <benjaminha14@gmail.com>
2021-01-29 12:16:42 -0800

    Avantianagral website dev done

---
+++ src/pages/avantinagral.tsx
@@ -0,0 +1,68 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import { GothamProps } from "../themes/Gotham";
+
+const Gotham = dynamic(import("scenes/Gotham"), { ssr: false });
+const ARTIST = {
+  name: "Avantinagral",
+  socials: [
+    "https://www.instagram.com/avantinagral/",
+    "https://open.spotify.com/artist/2Wwa2Sov84hVY7Hxfqu71Y?si=YBcFgxEaSYiizO_h3-OFHA",
+    "https://linktr.ee/avantinagral",
+  ],
+};
+
+const url = `https://d27rt3a60hh1lx.cloudfront.net/content/muse.place/avantinagral`;
+const artwork: GothamProps["artwork"] = [
+  {
+    src: `${url}/1.mp4`,
+    audio: true,
+    size: [1080, 1080],
+  },
+  {
+    src: `${url}/2.jpg`,
+    size: [1080, 1080],
+  },
+  {
+    src: `${url}/3.mp4`,
+    size: [1080, 1080],
+  },
+  {
+    src: `${url}/4.mp4`,
+    size: [1080, 1080],
+  },
+  {
+    src: `${url}/5.mp4`,
+    size: [1080, 1080],
+  },
+  {
+    src: `${url}/6.mp4`,
+    size: [1080, 1080],
+  },
+];
+
+const LinkTree: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Avantinagral</title>
+      </Head>
+      <Gotham
+        artwork={artwork}
+        socials={ARTIST.socials}
+        name={ARTIST.name}
+        map="city"
+        scenePos={[0, -20, 0]}
+        fogNear={0}
+        fogFar={150}
+        fogColor={"#000000"}
+        hMapScale={30}
+        xzMapScale={100}
+        floorColor="black"
+      ></Gotham>
+    </>
+  );
+};
+
+export default LinkTree;
