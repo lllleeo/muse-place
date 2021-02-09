6819baacf1afc0669ee5b83a0cc0df52f7a0b2df
Alex Shortt <alexander.shortt@gmail.com>
2021-02-08 20:45:58 -0800

    add gallery content, begin product panel

---
+++ src/scenes/Silks/components/ProductPanel/components/Content.tsx
@@ -0,0 +1,46 @@
+import { GroupProps } from "react-three-fiber";
+import { Text } from "@react-three/drei";
+import { Image } from "spacesvr";
+import { HEIGHT, WIDTH } from "../index";
+
+const TITLE_TEXT = {
+  fontSize: 0.03,
+  maxWidth: WIDTH / 2,
+  anchorX: "left",
+  anchorY: "top",
+};
+
+const DESC_TEXT = {
+  fontSize: 0.02,
+  maxWidth: TITLE_TEXT.maxWidth,
+  anchorX: "left",
+  anchorY: "top",
+};
+
+const PADDING = 0.1;
+const IMG_SIZE = 0.3;
+
+const Content = (props: GroupProps) => {
+  const PRODUCT = {
+    title: "22 neon green silk (green white)",
+    img:
+      "https://cdn.shopify.com/s/files/1/0249/8700/9086/products/IMG_1069_1024x1024.jpg",
+    description: "Black stitching + brush included",
+  };
+
+  return (
+    <group name="content" {...props}>
+      <Text {...TITLE_TEXT} position-y={HEIGHT / 2 - PADDING}>
+        {PRODUCT.title.toUpperCase()}
+      </Text>
+      <Text {...DESC_TEXT}>{PRODUCT.description}</Text>
+      <Image
+        src={PRODUCT.img}
+        size={[(582 / 871) * IMG_SIZE, IMG_SIZE]}
+        position-x={-WIDTH / 4}
+      />
+    </group>
+  );
+};
+
+export default Content;
--- src/scenes/Silks/index.tsx
+++ src/scenes/Silks/index.tsx
@@ -4,15 +4,22 @@ import { Vector3 } from "three";
 import Gallery from "./components/Gallery";
 import { Sky } from "@react-three/drei";
 import Cart from "./Cart";
+import ProductPanel from "./components/ProductPanel";
 
-const VIDEO_URL =
-  "https://d27rt3a60hh1lx.cloudfront.net/content/silksbyvp/video.mp4";
+const SILKS_CONTENT_FOLDER =
+  "https://d27rt3a60hh1lx.cloudfront.net/content/silksbyvp";
+
+const VIDEO_URL = `${SILKS_CONTENT_FOLDER}/video.mp4`;
 const VIDEO_SCALE = 2.2;
 
 const Silks = () => {
   return (
     <StandardEnvironment
-      player={{ pos: new Vector3(0, 1, 0), speed: 1.25 }}
+      player={{
+        pos: new Vector3(0, 1, 0),
+        speed: 1.25,
+        controls: { disableGyro: true },
+      }}
       canvasProps={{ gl: { antialias: true } }}
     >
       <Cart />
@@ -27,6 +34,7 @@ const Silks = () => {
       <SilksModel />
       <pointLight position={[0, 5, 0]} intensity={0.1} />
       <Sky sunPosition={[0, 1, 0.8]} />
+      <ProductPanel position={[-0.75, 0, -3.3]} />
     </StandardEnvironment>
   );
 };
