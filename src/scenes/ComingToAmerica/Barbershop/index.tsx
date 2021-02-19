8ec4c887708d162544ec54be7994b1303942fd4b
Alex Shortt <alexander.shortt@gmail.com>
2021-02-18 22:40:18 -0800

    push placeholder models for exterior + barbershop

--- src/scenes/ComingToAmerica/Barbershop/index.tsx
+++ src/scenes/ComingToAmerica/Barbershop/index.tsx
@@ -1,5 +1,21 @@
+import { Suspense } from "react";
+import { StandardEnvironment } from "spacesvr";
+import { Sky } from "@react-three/drei";
+import C2ABarbershop from "./models/C2ABarbershop";
+import { Vector3 } from "three";
+
 const Barbershop = () => {
-  return <>Barbershop</>;
+  return (
+    <StandardEnvironment player={{ pos: new Vector3(0, 1.25, 0), speed: 0.9 }}>
+      <Sky />
+      <Suspense fallback={null}>
+        <C2ABarbershop />
+      </Suspense>
+      <pointLight position={[10, 30, 10]} intensity={0.6} />
+      <pointLight position={[-10, 30, -10]} intensity={0.6} />
+      <ambientLight />
+    </StandardEnvironment>
+  );
 };
 
 export default Barbershop;
--- src/scenes/ComingToAmerica/Exterior/index.tsx
+++ src/scenes/ComingToAmerica/Exterior/index.tsx
@@ -1,5 +1,24 @@
-const Exterior = () => {
-  return <>Exterior</>;
+import { Suspense } from "react";
+import { StandardEnvironment } from "spacesvr";
+import { Sky } from "@react-three/drei";
+import C2AExterior from "./models/C2AExterior";
+import { Vector3 } from "three";
+
+const ComingAmerica = () => {
+  return (
+    <StandardEnvironment
+      player={{ pos: new Vector3(0, 2, 0), speed: 2 }}
+      disableGround
+    >
+      <Sky sunPosition={[0, 1, 0.8]} />
+      <Suspense fallback={null}>
+        <C2AExterior />
+      </Suspense>
+      <pointLight position={[10, 30, 10]} intensity={0.6} />
+      <pointLight position={[-10, 30, -10]} intensity={0.6} />
+      <ambientLight />
+    </StandardEnvironment>
+  );
 };
 
-export default Exterior;
+export default ComingAmerica;
