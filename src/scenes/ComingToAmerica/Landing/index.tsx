809a8a307c3a044361a44955f03bf7cdec8c9a6c
Isaiah <anyiminene@gmail.com>
2021-02-24 05:33:48 -0800

    portrait lock on mobile didnt work, done trying, will format later

--- src/scenes/ComingToAmerica/Landing/index.tsx
+++ src/scenes/ComingToAmerica/Landing/index.tsx
@@ -2,10 +2,6 @@ import Background from "./components/Background";
 import GlobalStyles from "./styles/GlobalStyles";
 import Content from "./components/Content";
 
-if (window.innerHeight < 501) {
-  screen.orientation.lock("portrait");
-}
-
 export const secondaryColor = "#935c23";
 export const bgColor = "#FDEFD1";
 
