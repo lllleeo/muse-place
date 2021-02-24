a80a3a7e58b3b9de91b315c2bf1e6b7a14cba648
Isaiah <anyiminene@gmail.com>
2021-02-24 05:23:11 -0800

    portrait lock on mobile?

--- src/scenes/ComingToAmerica/Landing/index.tsx
+++ src/scenes/ComingToAmerica/Landing/index.tsx
@@ -2,7 +2,9 @@ import Background from "./components/Background";
 import GlobalStyles from "./styles/GlobalStyles";
 import Content from "./components/Content";
 
-// export const mainColor = "#751D17";
+if (window.innerHeight < 501) {
+  screen.orientation.lock("portrait");
+}
 
 export const secondaryColor = "#935c23";
 export const bgColor = "#FDEFD1";
