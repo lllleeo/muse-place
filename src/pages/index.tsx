53963ebef67b5c3bc75632e6e670f6a46171464b
Alex Shortt <alexander.shortt@gmail.com>
2021-03-25 00:23:16 -0700

    remove files for jason launch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,33 @@ import { NextPage } from "next";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 
-const Landing = dynamic(import("scenes/Landing"), { ssr: false });
+const Gotham = dynamic(import("scenes/Gotham"), { ssr: false });
+const JasonMatias = dynamic(import("scenes/Gotham/JasonMatias"), {
+  ssr: false,
+});
 
-const Index: NextPage = () => {
+const LinkTree: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Muse</title>
+        <title>VR Gallery | Jason Matias</title>
       </Head>
-      <Landing />
+      <Gotham
+        socials={[]}
+        name=""
+        premium
+        emailCollection={{
+          title:
+            "Sign up to receive beautiful arts and new releases from the artist.",
+          link: "http://eepurl.com/gfW1JP",
+        }}
+        fbPixel="1756555791243957"
+        googleAnalytics="UA-178413400-1"
+      >
+        <JasonMatias />
+      </Gotham>
     </>
   );
 };
 
-export default Index;
+export default LinkTree;
