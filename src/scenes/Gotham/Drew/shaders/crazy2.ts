badd12e03c967857ed4137ee6f7c7f7c366af114
Isaiah <anyiminene@gmail.com>
2021-06-03 15:00:08 -0700

    removed unused shader code

--- src/scenes/Gotham/Drew/shaders/crazy2.ts
+++ src/scenes/Gotham/Drew/shaders/crazy2.ts
@@ -5,7 +5,6 @@ import { shaderMaterial } from "@react-three/drei";
 
 const CrazyMaterial = shaderMaterial(
   {
-    resolution: new Vector2(2000, 2000),
     time: 0,
   },
   `
@@ -19,27 +18,6 @@ const CrazyMaterial = shaderMaterial(
   `
   varying vec2 vUv;
   uniform float time;
-  uniform vec2 resolution;
-  vec2 Randomise_Fractal = vec2(0.572, 0.432);
-  float NUM_SIDES = 3.14567;
-  float PI = 3.14159265359;
-  
-  void koleidoscope(inout vec2 uv)
-  {
-    float KA = PI / NUM_SIDES;
-    // get the angle in radians of the current coords relative to origin (i.e. center of screen)
-    float angle = atan (uv.y, uv.x);
-    // repeat image over evenly divided rotations around the center
-    angle = mod (angle, 2.0 * KA);
-    // reflect the image within each subdivision to create a tilelable appearance
-    angle = abs (angle - KA);
-    // rotate image over time
-    angle += 0.2 *time;
-    // get the distance of the coords from the uv origin (i.e. center of the screen)
-    float d = length(uv); 
-    // map the calculated angle to the uv coordinate system at the given distance
-    uv = d * vec2(cos(angle), sin(angle));
-  }
   
   void main() {
     
