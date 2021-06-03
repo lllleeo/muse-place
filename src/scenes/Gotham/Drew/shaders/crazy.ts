1bb0b649bbc4c3c20f58c0d58df588a7040be52a
Isaiah <anyiminene@gmail.com>
2021-06-02 21:57:53 -0700

    sky started

--- src/scenes/Gotham/Drew/shaders/crazy.ts
+++ src/scenes/Gotham/Drew/shaders/crazy.ts
@@ -42,41 +42,51 @@ const CrazyMaterial = shaderMaterial(
   }
   
   void main() {
-    // vec4 color = texture2D( tDiffuse, vUv );
+    // // vec4 color = texture2D( tDiffuse, vUv );
+    //
+    // float KA = PI / NUM_SIDES;
+    // float time = time * 0.7;
+    // float scale = 10.0 + 20.0 * sin(time * 0.00008);
+    // float r1 = 0.4 ; 
+    // float r2 = 0.9 ; 
+    // float r3 = 0.29; 
+    //
+    // float h = resolution.y;
+    // float w = resolution.x;
+    //
+    // vec2 uv = 12.0*(2.0 * vUv.xy  - 1.0);
+    // koleidoscope(uv);
+    //
+    // float x = uv.x;
+    // float y = uv.y;
+    //
+    //  float col = 
+    //     sin(distance( vec2(x * r1 + time, y * r2), vec2(w / r3 , h) ) * scale) +
+    //     sin(distance( vec2(x, y * r2), vec2(1.0 / h * r3, w * r1) ) * scale) +
+    //     sin(distance( vec2(r3 * x + time, r1 * y + time), vec2(w * r2 + h * r1, h * r2) ) * scale) +
+    //     sin(distance( vec2(1.0 / x * r3 , y * r2), vec2(h, w) ) * scale);    
+    //
+    // vec3 color = vec3( 0.5 + 0.5 * sin(col), cos(col * 100.0) * 0.2, cos(col) - sin(col)) + 0.1;
+    // color += mod(gl_FragColor.x, 2.0) < 1.0 ? 0.25 : 0.4;             
+    //
+    // vec3 p = vec3 (uv.x * 2.0, uv.y, Randomise_Fractal.x);
+    //   for (int i = 0; i < 44; i++)
+    //     p.xzy = vec3(1.1,0.999,0.678)*(abs((abs(p)/dot(p,p)-vec3(1.0,1.02,Randomise_Fractal.y*0.4))));
+    //    
+    // vec3 finalColor = vec3(p.x * color.z, p.y * 0.8 * color.y, p.z); 
+    // float gray = dot(finalColor.rgb, vec3(0.999, 0.587, 0.114));             
+    //
+    // gl_FragColor = vec4(0.851*gray, 0.012*gray*color.y + 0.15, p.z - 0.5, 1.0);
     
-    float KA = PI / NUM_SIDES;
-    float time = time * 0.7;
-    float scale = 10.0 + 20.0 * sin(time * 0.00008);
-    float r1 = 0.4 ; 
-    float r2 = 0.9 ; 
-    float r3 = 0.29; 
+    float r1 = 0.4;
+    float r2 = 0.9;
+    float r3 = 0.29;
     
-    float h = resolution.y;
-    float w = resolution.x;
-    
-    vec2 uv = 12.0*(2.0 * vUv.xy  - 1.0);
-    koleidoscope(uv);
-    
-    float x = uv.x;
-    float y = uv.y;
-
-     float col = 
-        sin(distance( vec2(x * r1 + time, y * r2), vec2(w / r3 , h) ) * scale) +
-        sin(distance( vec2(x, y * r2), vec2(1.0 / h * r3, w * r1) ) * scale) +
-        sin(distance( vec2(r3 * x + time, r1 * y + time), vec2(w * r2 + h * r1, h * r2) ) * scale) +
-        sin(distance( vec2(1.0 / x * r3 , y * r2), vec2(h, w) ) * scale);    
-    
-    vec3 color = vec3( 0.5 + 0.5 * sin(col), cos(col * 100.0) * 0.2, cos(col) - sin(col)) + 0.1;
-    color += mod(gl_FragColor.x, 2.0) < 1.0 ? 0.25 : 0.4;             
-
-    vec3 p = vec3 (uv.x * 2.0, uv.y, Randomise_Fractal.x);
-      for (int i = 0; i < 44; i++)
-        p.xzy = vec3(1.1,0.999,0.678)*(abs((abs(p)/dot(p,p)-vec3(1.0,1.02,Randomise_Fractal.y*0.4))));
-        
-    vec3 finalColor = vec3(p.x * color.z, p.y * 0.8 * color.y, p.z); 
-    float gray = dot(finalColor.rgb, vec3(0.999, 0.587, 0.114));             
-    
-    gl_FragColor = vec4(0.851*gray, 0.012*gray*color.y + 0.15, p.z - 0.5, 1.0);
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+    vec2 pos = (uv.xy-0.5);
+    vec2 cir = ((pos.xy*pos.xy+sin(uv.x*18.0+time)/25.0*sin(uv.y*7.0+time*1.5)/1.0)+uv.x*sin(time)/16.0+uv.y*sin(time*1.2)/16.0);
+    float circles = (sqrt(abs(cir.x+cir.y*0.5)*25.0)*5.0);
+    gl_FragColor = vec4(sin(circles*1.25+2.0),abs(sin(circles*1.0-1.0)-sin(circles)),abs(sin(circles)*1.0),1.0);
   }
 `
 );
