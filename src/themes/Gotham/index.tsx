1994d7b01bb9bb92f2f1d5799b86166459dc451c
Alex Shortt <alexander.shortt@gmail.com>
2021-06-10 02:47:50 -0700

    add onboarding to all sites, disable gyroscope, add fb analytics

--- src/themes/Gotham/index.tsx
+++ src/themes/Gotham/index.tsx
@@ -14,6 +14,7 @@ import EmailCollection, {
 import Credits from "./components/Credits";
 import FBPixel from "../components/FacebookPixel";
 import GoogleAnalytics from "../components/GoogleAnalytics";
+import Onboarding from "../components/Onboarding";
 
 export type GothamProps = {
   name: string;
@@ -65,6 +66,7 @@ export default function Gotham(props: GothamProps) {
           {...emailCollection}
         />
       )}
+      <Onboarding />
       <Preload all />
       <ambientLight intensity={1} />
       <Suspense fallback={null}>
