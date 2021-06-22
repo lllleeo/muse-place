f9bb630446b6b59c1606e45aea1b972cca4605d3
veltsonbastien <bastienveltson@outlook.com>
2021-06-21 22:05:53 -0700

    kris kites test version

--- src/scenes/Gotham/KKites/index.tsx
+++ src/scenes/Gotham/KKites/index.tsx
@@ -163,287 +163,287 @@ export default function KKites() {
           IFAKEMAKECLOTHES.COM
         </Text>
       </group>
-      {!isMobile && (
-        <group>
-          <group
-            position={[2.49, 1.25, 10.0]}
-            rotation-y={-Math.PI / 2}
-            name="back-wall-right"
-          >
-            <Image
-              name="r1"
-              src={`${CONTENT_FOLDER}/r1.jpg`}
-              position={[0.15, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="r2"
-              src={`${CONTENT_FOLDER}/r2.jpg`}
-              position={[1.3, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="r3"
-              src={`${CONTENT_FOLDER}/r3.jpg`}
-              position={[0.15, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="r4"
-              src={`${CONTENT_FOLDER}/r4.jpg`}
-              position={[1.3, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-          </group>
-          <group
-            position={[2.49, 1.25, -3.0]}
-            rotation-y={-Math.PI / 2}
-            name="back-wall-left"
-          >
-            <Image
-              name="l1"
-              src={`${CONTENT_FOLDER}/l1.jpg`}
-              position={[0.15, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="l2"
-              src={`${CONTENT_FOLDER}/l2.jpg`}
-              position={[1.3, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="l3"
-              src={`${CONTENT_FOLDER}/l3.jpg`}
-              position={[0.15, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="l4"
-              src={`${CONTENT_FOLDER}/l4.jpg`}
-              position={[1.3, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-          </group>
-          <group
-            position={[-5.47, 1.25, 0.7]}
-            rotation-y={Math.PI / 2}
-            name="front-wall-right"
-          >
-            {/*DON'T DELETE UNTIL AFTER CONFIRMING WITH KRIS KITES*/}
-            {/*<Image*/}
-            {/*    name="l5"*/}
-            {/*    src={`${CONTENT_FOLDER}/l5.jpg`}*/}
-            {/*    position={[0.15, 0.5, 0]}*/}
-            {/*    size={1.15}*/}
-            {/*    framed*/}
-            {/*/>*/}
-            {/*<Image*/}
-            {/*    name="l6"*/}
-            {/*    src={`${CONTENT_FOLDER}/l6.jpg`}*/}
-            {/*    position={[1.3, 0.5, 0]}*/}
-            {/*    size={1.15}*/}
-            {/*    framed*/}
-            {/*/>*/}
-            {/*<Image*/}
-            {/*    name="l7"*/}
-            {/*    src={`${CONTENT_FOLDER}/l7.jpg`}*/}
-            {/*    position={[2.45, 0.5, 0]}*/}
-            {/*    size={1.15}*/}
-            {/*    framed*/}
-            {/*/>*/}
-            {/*<Image*/}
-            {/*    name="l8"*/}
-            {/*    src={`${CONTENT_FOLDER}/l8.jpg`}*/}
-            {/*    position={[0.15, -0.75, 0]}*/}
-            {/*    size={1.15}*/}
-            {/*    framed*/}
-            {/*/>*/}
-            {/*<Image*/}
-            {/*    name="l9"*/}
-            {/*    src={`${CONTENT_FOLDER}/l9.jpg`}*/}
-            {/*    position={[1.3, -0.75, 0]}*/}
-            {/*    size={1.15}*/}
-            {/*    framed*/}
-            {/*/>*/}
-            {/*<Image*/}
-            {/*    name="l10"*/}
-            {/*    src={`${CONTENT_FOLDER}/l10.jpg`}*/}
-            {/*    position={[2.45, -0.75, 0]}*/}
-            {/*    size={1.15}*/}
-            {/*    framed*/}
-            {/*/>*/}
-            <Image
-              name="11"
-              src={`${CONTENT_FOLDER}/11.jpg`}
-              position={[0.15, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="12"
-              src={`${CONTENT_FOLDER}/12.jpg`}
-              position={[1.3, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="13"
-              src={`${CONTENT_FOLDER}/13.jpg`}
-              position={[2.45, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="14"
-              src={`${CONTENT_FOLDER}/14.jpg`}
-              position={[0.15, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="15"
-              src={`${CONTENT_FOLDER}/15.jpg`}
-              position={[1.3, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="16"
-              src={`${CONTENT_FOLDER}/16.jpg`}
-              position={[2.45, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="5"
-              src={`${CONTENT_FOLDER}/5.jpg`}
-              position={[-1, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="6"
-              src={`${CONTENT_FOLDER}/6.jpg`}
-              position={[-2.15, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="7"
-              src={`${CONTENT_FOLDER}/7.jpg`}
-              position={[-1, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="8"
-              src={`${CONTENT_FOLDER}/8.jpg`}
-              position={[-2.15, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="9"
-              src={`${CONTENT_FOLDER}/9.jpg`}
-              position={[3.6, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="10"
-              src={`${CONTENT_FOLDER}/10.jpg`}
-              position={[3.6, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-          </group>
-          <group
-            position={[-5.47, 1.25, 10.66]}
-            rotation-y={Math.PI / 2}
-            name="front-wall-left"
-          >
-            <Image
-              name="17"
-              src={`${CONTENT_FOLDER}/17.jpg`}
-              position={[-1, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="27"
-              src={`${CONTENT_FOLDER}/27.jpg`}
-              position={[-1, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="19"
-              src={`${CONTENT_FOLDER}/19.jpg`}
-              position={[0.15, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="29"
-              src={`${CONTENT_FOLDER}/29.jpg`}
-              position={[1.3, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="21"
-              src={`${CONTENT_FOLDER}/21.jpg`}
-              position={[2.5, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="22"
-              src={`${CONTENT_FOLDER}/22.jpg`}
-              position={[0.25, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="30"
-              src={`${CONTENT_FOLDER}/30.jpg`}
-              position={[1.48, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="24"
-              src={`${CONTENT_FOLDER}/24.jpg`}
-              position={[2.6, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="32"
-              src={`${CONTENT_FOLDER}/32.jpg`}
-              position={[3.7, 0.5, 0]}
-              size={1.15}
-              framed
-            />
-            <Image
-              name="26"
-              src={`${CONTENT_FOLDER}/26.jpg`}
-              position={[3.78, -0.75, 0]}
-              size={1.15}
-              framed
-            />
-          </group>
+      {/*{!isMobile && (*/}
+      <group>
+        <group
+          position={[2.49, 1.25, 10.0]}
+          rotation-y={-Math.PI / 2}
+          name="back-wall-right"
+        >
+          <Image
+            name="r1"
+            src={`${CONTENT_FOLDER}/r1.jpg`}
+            position={[0.15, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="r2"
+            src={`${CONTENT_FOLDER}/r2.jpg`}
+            position={[1.3, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="r3"
+            src={`${CONTENT_FOLDER}/r3.jpg`}
+            position={[0.15, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="r4"
+            src={`${CONTENT_FOLDER}/r4.jpg`}
+            position={[1.3, -0.75, 0]}
+            size={1.15}
+            framed
+          />
         </group>
-      )}
+        <group
+          position={[2.49, 1.25, -3.0]}
+          rotation-y={-Math.PI / 2}
+          name="back-wall-left"
+        >
+          <Image
+            name="l1"
+            src={`${CONTENT_FOLDER}/l1.jpg`}
+            position={[0.15, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="l2"
+            src={`${CONTENT_FOLDER}/l2.jpg`}
+            position={[1.3, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="l3"
+            src={`${CONTENT_FOLDER}/l3.jpg`}
+            position={[0.15, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="l4"
+            src={`${CONTENT_FOLDER}/l4.jpg`}
+            position={[1.3, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+        </group>
+        <group
+          position={[-5.47, 1.25, 0.7]}
+          rotation-y={Math.PI / 2}
+          name="front-wall-right"
+        >
+          {/*DON'T DELETE UNTIL AFTER CONFIRMING WITH KRIS KITES*/}
+          {/*<Image*/}
+          {/*    name="l5"*/}
+          {/*    src={`${CONTENT_FOLDER}/l5.jpg`}*/}
+          {/*    position={[0.15, 0.5, 0]}*/}
+          {/*    size={1.15}*/}
+          {/*    framed*/}
+          {/*/>*/}
+          {/*<Image*/}
+          {/*    name="l6"*/}
+          {/*    src={`${CONTENT_FOLDER}/l6.jpg`}*/}
+          {/*    position={[1.3, 0.5, 0]}*/}
+          {/*    size={1.15}*/}
+          {/*    framed*/}
+          {/*/>*/}
+          {/*<Image*/}
+          {/*    name="l7"*/}
+          {/*    src={`${CONTENT_FOLDER}/l7.jpg`}*/}
+          {/*    position={[2.45, 0.5, 0]}*/}
+          {/*    size={1.15}*/}
+          {/*    framed*/}
+          {/*/>*/}
+          {/*<Image*/}
+          {/*    name="l8"*/}
+          {/*    src={`${CONTENT_FOLDER}/l8.jpg`}*/}
+          {/*    position={[0.15, -0.75, 0]}*/}
+          {/*    size={1.15}*/}
+          {/*    framed*/}
+          {/*/>*/}
+          {/*<Image*/}
+          {/*    name="l9"*/}
+          {/*    src={`${CONTENT_FOLDER}/l9.jpg`}*/}
+          {/*    position={[1.3, -0.75, 0]}*/}
+          {/*    size={1.15}*/}
+          {/*    framed*/}
+          {/*/>*/}
+          {/*<Image*/}
+          {/*    name="l10"*/}
+          {/*    src={`${CONTENT_FOLDER}/l10.jpg`}*/}
+          {/*    position={[2.45, -0.75, 0]}*/}
+          {/*    size={1.15}*/}
+          {/*    framed*/}
+          {/*/>*/}
+          <Image
+            name="11"
+            src={`${CONTENT_FOLDER}/11.jpg`}
+            position={[0.15, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="12"
+            src={`${CONTENT_FOLDER}/12.jpg`}
+            position={[1.3, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="13"
+            src={`${CONTENT_FOLDER}/13.jpg`}
+            position={[2.45, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="14"
+            src={`${CONTENT_FOLDER}/14.jpg`}
+            position={[0.15, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="15"
+            src={`${CONTENT_FOLDER}/15.jpg`}
+            position={[1.3, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="16"
+            src={`${CONTENT_FOLDER}/16.jpg`}
+            position={[2.45, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="5"
+            src={`${CONTENT_FOLDER}/5.jpg`}
+            position={[-1, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="6"
+            src={`${CONTENT_FOLDER}/6.jpg`}
+            position={[-2.15, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="7"
+            src={`${CONTENT_FOLDER}/7.jpg`}
+            position={[-1, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="8"
+            src={`${CONTENT_FOLDER}/8.jpg`}
+            position={[-2.15, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="9"
+            src={`${CONTENT_FOLDER}/9.jpg`}
+            position={[3.6, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="10"
+            src={`${CONTENT_FOLDER}/10.jpg`}
+            position={[3.6, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+        </group>
+        <group
+          position={[-5.47, 1.25, 10.66]}
+          rotation-y={Math.PI / 2}
+          name="front-wall-left"
+        >
+          <Image
+            name="17"
+            src={`${CONTENT_FOLDER}/17.jpg`}
+            position={[-1, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="27"
+            src={`${CONTENT_FOLDER}/27.jpg`}
+            position={[-1, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="19"
+            src={`${CONTENT_FOLDER}/19.jpg`}
+            position={[0.15, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="29"
+            src={`${CONTENT_FOLDER}/29.jpg`}
+            position={[1.3, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="21"
+            src={`${CONTENT_FOLDER}/21.jpg`}
+            position={[2.5, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="22"
+            src={`${CONTENT_FOLDER}/22.jpg`}
+            position={[0.25, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="30"
+            src={`${CONTENT_FOLDER}/30.jpg`}
+            position={[1.48, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="24"
+            src={`${CONTENT_FOLDER}/24.jpg`}
+            position={[2.6, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="32"
+            src={`${CONTENT_FOLDER}/32.jpg`}
+            position={[3.7, 0.5, 0]}
+            size={1.15}
+            framed
+          />
+          <Image
+            name="26"
+            src={`${CONTENT_FOLDER}/26.jpg`}
+            position={[3.78, -0.75, 0]}
+            size={1.15}
+            framed
+          />
+        </group>
+      </group>
+      {/* )} */}
       <group name="outside-videos" position={[-1.82, 1, 0]}>
         <Video
           src={`${CONTENT_FOLDER}/jpiece.mp4`}
