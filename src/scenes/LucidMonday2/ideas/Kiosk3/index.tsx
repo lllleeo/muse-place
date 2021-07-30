57b7ca452e6f92110dbe41882d7ae9a5f2e89196
Isaiah <anyiminene@gmail.com>
2021-07-30 04:38:20 -0700

    kiosk 3 for discord added

--- src/scenes/LucidMonday2/ideas/Kiosk3/index.tsx
+++ src/scenes/LucidMonday2/ideas/Kiosk3/index.tsx
@@ -1,5 +1,5 @@
 import SocialButton from "themes/components/SocialButton";
-import Content from "./models/Content4";
+import Content from "./models/Content3";
 import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
 import { Floating } from "spacesvr";
@@ -20,29 +20,11 @@ export default function Kiosk3() {
 
   return (
     <group>
-      <group ref={group1} position={[0, 10, -12]}>
+      <group ref={group1} position={[11, 15, -0]}>
         <Content />
         <Floating height={0.02} speed={2}>
-          <group position={[0, 0.85, 0.2]} scale={0.75}>
-            <SocialButton link="https://www.instagram.com/lucidmonday" />
-            <SocialButton
-              link="https://twitter.com/LucidMonday"
-              position-x={1}
-            />
-            <SocialButton
-              link="https://soundcloud.com/lucidmonday"
-              position-x={2}
-            />
-            <SocialButton
-              link="https://www.youtube.com/channel/UCyzAwg6jela6dNhe38Rwp7Q"
-              position={[-1.25, 0, -1]}
-              rotation-y={-Math.PI / 2}
-            />
-            <SocialButton
-              link="https://open.spotify.com/artist/7dhK4qWq1jEEFjXCD5z5xr"
-              position={[-1.175, 0, -2]}
-              rotation-y={-Math.PI / 2}
-            />
+          <group position={[0, 0.75, 0.2]} scale={0.75}>
+            <SocialButton link="https://discord.com/invite/xQspThf" />
           </group>
         </Floating>
       </group>
